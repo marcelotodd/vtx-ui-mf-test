161a71f1a99fc67088efdec3c61522531dc41750
Michael Ellis <michael.ellis@vertexinc.com>
2020-09-16 08:50:59 -0400

    Initial commit

---
+++ src/root.component.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+
+interface OwnProps {
+  name: string;
+  loginUser: any;
+}
+
+interface ComponentState {
+  requests: number;
+}
+
+export class Root extends React.Component<OwnProps, ComponentState> {
+  private userInfo: any;
+
+  constructor(props: OwnProps) {
+    super(props);
+    this.state = { requests: 0 };
+  }
+
+  componentDidUpdate(prevProps: OwnProps, prevState: ComponentState) {
+    if (prevState.requests != this.state.requests) {
+      this.setState({ requests: 0 });
+    }
+  }
+
+  render() {
+    const { name, loginUser } = this.props;
+
+    return (
+      <div className="vtx-ui-mf-test">
+        <h2>{name} microfrontend</h2>
+        {!loginUser ? (
+          <div>No user provided. Not good!</div>
+        ) : (
+          <pre>{JSON.stringify(loginUser, null, 2)}</pre>
+        )}
+        <div style={{ marginTop: "20px" }}>
+          <button
+            type="button"
+            onClick={() => {
+              const that = this;
+              fetch("http://kopas0036:9000/oseries-auth/user", {
+                headers: {
+                  Authorization: `Bearer ${loginUser.access_token}`,
+                },
+              })
+                .then((response) => response.json())
+                .then((userInfo) => {
+                  that.userInfo = userInfo;
+                  that.setState({ requests: 1 });
+                });
+            }}
+          >
+            Get User Info
+          </button>
+          {this.userInfo != null && (
+            <pre>{JSON.stringify(this.userInfo, null, 2)}</pre>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "vtx-ui",
+    projectName: "mf-test",
+    webpackConfigEnv,
+  });
+
+  return webpackMerge.smart(
+    {
+      ...defaultConfig,
+      entry: path.resolve(process.cwd(), `src/vtx-ui-mf-test.tsx`),
+      output: {
+        filename: `vtx-ui-mf-test.js`,
+        libraryTarget: "system",
+        path: path.resolve(process.cwd(), "build"),
+        jsonpFunction: `webpackJsonp_vtx-ui-mf-test`,
+      },
+      externals: ["single-spa"],
+    },
+    {
+      // modify the webpack config however you'd like to by adding to this object
+    }
+  );
+};
